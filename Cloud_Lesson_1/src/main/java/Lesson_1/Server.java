41b3e91f30eac6f38888859c1faaaa55c1b85ead
Vitaliy <Pilipenko.va@live.ru>
2021-06-17 15:03:16 +0300

    Копия проекта

---
+++ Cloud_Lesson_1/src/main/java/Lesson_1/Server.java
@@ -0,0 +1,26 @@
+package Lesson_1;
+
+import java.net.ServerSocket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    // TODO: 14.06.2021
+    // организовать корректный вывод статуса
+    // подумать почему так реализован цикл в Lesson_1.ClientHandler
+    public Server() {
+        ExecutorService service = Executors.newFixedThreadPool(4);
+        try (ServerSocket server = new ServerSocket(5679)){
+            System.out.println("Lesson_1.Server started");
+            while (true) {
+                service.execute(new ClientHandler(server.accept()));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Server();
+    }
+}
